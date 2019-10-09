98f07257df152571baea73a4b7f840a4444af3f6
Pierre <pgfuentes@yahoo.com>
2019-10-09 13:00:13 +0000

    linkedlist

--- blur2.rb
+++ blur2.rb
@@ -23,10 +23,10 @@ class Image
           ones.each do |found_row_number, found_col_number|
 
             if row_number == found_row_number && col_number == found_col_number
-              @picture[row_number -1][col_number] = 1 unless row_number == 0 #up
-              @picture[row_number +1][col_number] = 1 unless row_number >= 3 #down
-              @picture[row_number][col_number -1] = 1 unless col_number == 0 #left
-              @picture[row_number][col_number +1] = 1 unless col_number >= 3 #right
+              @picture[row_number -1][col_number] = 1 unless row_number == 0 
+              @picture[row_number +1][col_number] = 1 unless row_number >= 3 
+              @picture[row_number][col_number -1] = 1 unless col_number == 0 
+              @picture[row_number][col_number +1] = 1 unless col_number >= 3 
             end
           end
         end
@@ -47,10 +47,9 @@ image = Image.new([
 [0, 0, 0, 0]
 ])
 
-#original output
+
 image.output_image
 puts
 image.blur!
 
-#blurred output
 image.output_image
--- blur3.rb
+++ blur3.rb
@@ -33,7 +33,6 @@ class Image
   def manhattan_distance (x1, y1, x2, y2)
     horizontal_distance = (x2 - x1).abs
     vertical_distance = (y2 - y1).abs
-    # (x2 - x1).abs + (y2 - y1).abs
     horizontal_distance + vertical_distance
   end
 
@@ -54,10 +53,8 @@ image = Image.new([
 [0, 0, 0, 0, 0, 1]
 ])
 
-# original image
 image.output_image
 puts
 
-# blurred image
 image.blur!(2)
 image.output_image
